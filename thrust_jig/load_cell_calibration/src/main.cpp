8535c4fbcdfad36ccb639406dda7ee2045627772
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2023-01-18 14:35:47 -0800

    setup PlatformIO project for static thrust/torque jig

---
+++ thrust_jig/load_cell_calibration/src/main.cpp
@@ -0,0 +1,13 @@
+#include "HX711.h"
+
+static HX711 load_cell;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
