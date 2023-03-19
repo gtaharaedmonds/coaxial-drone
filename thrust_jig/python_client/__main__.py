540fc793d8997f20bb82169bec1232c5cd39f7ec
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2023-03-19 15:12:20 -0700

    torque v angle

--- thrust_jig/python_client/__main__.py
+++ thrust_jig/python_client/__main__.py
@@ -11,14 +11,14 @@ def connect_to_device(port: str, timeout=10):
     Connect to device on port, throw an error on failure.
     """
 
-    return serial.Serial(port, baudrate=9600, timeout=timeout)
+    return serial.Serial(port, baudrate=115200, timeout=timeout)
 
 
 def load_settings(calibration_path: Path):
     """
     Loads most recent calibration file for a given device id
 
-    Returns (channel_names, offsets, scales)
+    Returns (channel_names, offsets, scales, units)
     """
 
     with calibration_path.open() as calibration_file:
@@ -28,24 +28,26 @@ def load_settings(calibration_path: Path):
         next(csv_rows)
 
         # For each of the next rows, ignore the first column and convert datatypes if required
-        channel_names = next(csv_rows)[0].split()[1:]
-        offsets = [float(offset) for offset in next(csv_rows)[0].split()[1:]]
-        scales = [float(scale) for scale in next(csv_rows)[0].split()[1:]]
+        channel_names = next(csv_rows)[1:]
+        offsets = [float(offset) for offset in next(csv_rows)[1:]]
+        scales = [float(scale) for scale in next(csv_rows)[1:]]
+        units = next(csv_rows)[1:]
 
-        return channel_names, offsets, scales, calibration_path
+        return channel_names, offsets, scales, units
 
 
 def run_experiment(port: str, experiment_dir: Path, calibration_path: Path):
     """Main program flow"""
     ser = connect_to_device(port)
 
-    channel_names, offsets, scales, calibration_path = load_settings(calibration_path)
+    channel_names, offsets, scales, units = load_settings(
+        calibration_path
+    )
 
     local_tz = datetime.now().astimezone().tzinfo
     file_time = datetime.now(tz=local_tz)
-    with open(
-        f"{experiment_dir}/{file_time:%Y-%m-%d_%H-%M-%S}.csv", "w", newline=""
-    ) as csv_file:
+    file_name = f"{file_time:%Y-%m-%d_%H-%M-%S}"
+    with open(f"{experiment_dir}/{file_name}.csv", "w", newline="") as csv_file:
         csv_writer = csv.writer(csv_file)
 
         # Write file header
@@ -59,7 +61,7 @@ def run_experiment(port: str, experiment_dir: Path, calibration_path: Path):
             [*channel_names, *(f"{name}_raw" for name in channel_names)]
         )
 
-        # Clear RX buffer
+        # Flush RX buffer
         ser.flush()
 
         while True:
@@ -72,6 +74,14 @@ def run_experiment(port: str, experiment_dir: Path, calibration_path: Path):
             ]
 
             csv_writer.writerow([*calibrated_readings, *raw_readings])
+            print(
+                ", ".join(
+                    "{:}: {:6.2f} {:}".format(name, value, unit)
+                    for name, value, unit in zip(
+                        channel_names, calibrated_readings, units
+                    )
+                )
+            )
 
 
 if __name__ == "__main__":
@@ -82,7 +92,7 @@ if __name__ == "__main__":
         description="Reads data from load cells in the force measurement jig",
     )
     parser.add_argument(
-        "port", metavar="P", help=f"Serial port, found: {available_ports}"
+        "port", metavar="P", help=f"Serial port, found: {', '.join(available_ports)}"
     )
     parser.add_argument(
         "--experiment-dir",
