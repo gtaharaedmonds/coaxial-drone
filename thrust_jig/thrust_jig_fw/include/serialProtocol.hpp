5337bb8728ffd63a696dae0fcaa0e0b8d9036245
Nicholas Carr <ncarr@users.noreply.github.com>
2023-11-05 20:11:32 -0800

    Implement serial API

---
+++ thrust_jig/thrust_jig_fw/include/serialProtocol.hpp
@@ -0,0 +1,27 @@
+#ifndef SERIALPROTOCOL_H_
+#define SERIALPROTOCOL_H_
+
+/* Includes */
+#include <deque>
+#include <HardwareSerial.h>
+
+/* Symbolic constants */
+#define NUM_COMMAND_SET_VALUES 5
+
+/* Type definitions */
+struct CommandSet_S
+{
+    unsigned long time_ms;
+    double top_percent;
+    double bot_percent;
+    int pitch_us;
+    int roll_us;
+};
+
+/* Static variables */
+static std::deque<CommandSet_S> test_spec;
+
+/* Function definitions */
+void receiveTestSpec(void);
+
+#endif
--- thrust_jig/thrust_jig_fw/src/main.cpp
+++ thrust_jig/thrust_jig_fw/src/main.cpp
@@ -1,34 +1,89 @@
 #include <stdint.h>
 
-#include "HX711.h"
 #include "bldcMotor.hpp"
+#include "servoMotor.hpp"
 #include "loadCell.hpp"
+#include "powerMonitor.hpp"
+#include "serialProtocol.hpp"
 
 void setup()
 {
     Serial.begin(115200);
     Serial.println("Thrust Jig Firmware Program");
 
-    Serial.println("Setting up Motor");
+    Serial.println("Setting up");
+    setupBldcMotor(MOTOR_1);
     setupBldcMotor(MOTOR_2);
-    delay(100);
-
-    Serial.println("Arming Motor");
-    armBldcMotor(MOTOR_2);
+    setupServo(PITCH_VANE);
+    setupServo(ROLL_VANE);
+    setupLoadCell(THRUST);
+    setupLoadCell(TORQUE);
+    setupPowerMonitor();
 }
 
-static int loop_i = 0;
+enum ProgramState_E {
+    SETUP_TEST,
+    RUN_TEST
+};
+static ProgramState_E state = SETUP_TEST;
+static unsigned long start_time_micros = 0;
+static unsigned long start_time_millis = 0;
+static CommandSet_S current_command;
+static String input = "";
 
 void loop()
 {
-    if (loop_i < 10000) {
-        setThrottle(MOTOR_2, 50);
-        Serial.println(getRpm(MOTOR_2));
-    }
-    else if (loop_i == 10000) {
-        stopBldcMotor(MOTOR_2);
-    }
+    switch (state)
+    {
+    case SETUP_TEST:
+        receiveTestSpec();
+        armBldcMotor(ALL);
+        start_time_micros = micros();
+        start_time_millis = millis();
+        Serial.println("Starting test");
+        Serial.println("time_us,top_rpm,bot_rpm,v_bat,i_bat,i_top,i_bot,thrust_N,torque_Nm");
+        state = RUN_TEST;
+        break;
     
-    loop_i++;
-    delay(1);
+    case RUN_TEST:
+        if (test_spec.size() > 1 && test_spec.at(1).time_ms < millis() - start_time_millis) {
+            test_spec.pop_front();
+        }
+        current_command = test_spec.front();
+        setThrottlePercent(MOTOR_1, current_command.top_percent);
+        setThrottlePercent(MOTOR_2, current_command.bot_percent);
+        setServoPwm(PITCH_VANE, current_command.pitch_us);
+        setServoPwm(ROLL_VANE, current_command.roll_us);
+        Serial.printf(
+            "%lu,%" PRIu16 ",%" PRIu16 ",%f,%f,%f,%f,%f,%f\n",
+            micros() - start_time_micros,
+            getRpm(MOTOR_1),
+            getRpm(MOTOR_2),
+            getVoltageV(VBAT),
+            getCurrentA(IBAT),
+            getCurrentA(IMOTOR1),
+            getCurrentA(IMOTOR2),
+            getLoadCellValue(THRUST),
+            getLoadCellValue(TORQUE)
+        );
+        // Nonblocking string read persistent over multiple loop iterations
+        while (Serial.available()) {
+            char c = Serial.read();
+            if (c == '\n' && input != "Stop") {
+                input = "";
+            } else if (c >= 0 && c != '\n') {
+                input += c;
+            }
+        }
+        if (test_spec.size() <= 1 || (Serial.available() && input == "Stop")) {
+            input = "";
+            stopBldcMotor(ALL);
+            setServoPwm(PITCH_VANE, 1500);
+            setServoPwm(ROLL_VANE, 1500);
+            state = SETUP_TEST;
+            Serial.println("Stopped");
+        }
+        delay(1);
+        break;
+    }
 }
